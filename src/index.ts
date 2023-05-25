7df5dd8f998016c9d659dbb22ee9837e357b2ba4
yarzzc <98455501+yarzzc@users.noreply.github.com>
2023-05-25 19:00:09 +0800

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,272 @@
+import {Plugin, showMessage, confirm, Dialog, Menu, isMobile, openTab, adaptHotkey} from "siyuan";
+import "./index.scss";
+
+const STORAGE_NAME = "menu-config";
+const TAB_TYPE = "custom_tab";
+const DOCK_TYPE = "dock_tab";
+
+export default class PluginSample extends Plugin {
+
+    private customTab: () => any;
+
+    onload() {
+        this.data[STORAGE_NAME] = {readonlyText: "Readonly"};
+
+        const topBarElement = this.addTopBar({
+            icon: "iconEmoji",
+            title: this.i18n.addTopBarIcon,
+            position: "right",
+            callback: () => {
+                this.addMenu(topBarElement.getBoundingClientRect());
+            }
+        });
+
+        this.customTab = this.addTab({
+            type: TAB_TYPE,
+            init() {
+                this.element.innerHTML = `<div class="plugin-sample__custom-tab">${this.data.text}</div>`;
+            },
+            destroy() {
+                console.log("destroy tab:", TAB_TYPE);
+            }
+        });
+
+        this.addDock({
+            config: {
+                position: "LeftBottom",
+                size: {width: 200, height: 0},
+                icon: "iconEmoji",
+                title: "Custom Dock",
+            },
+            data: {
+                text: "This is my custom dock"
+            },
+            type: DOCK_TYPE,
+            init() {
+                this.element.innerHTML = `<div class="fn__flex-1 fn__flex-column">
+    <div class="block__icons">
+        <div class="block__logo">
+            <svg><use xlink:href="#iconEmoji"></use></svg>
+            Custom Dock
+        </div>
+        <span class="fn__flex-1 fn__space"></span>
+        <span data-type="min" class="block__icon b3-tooltips b3-tooltips__sw" aria-label="Min ${adaptHotkey("⌘W")}"><svg><use xlink:href="#iconMin"></use></svg></span>
+    </div>
+    <div class="fn__flex-1 plugin-sample__custom-dock">
+        ${this.data.text}
+    </div>
+</div>`;
+            },
+            destroy() {
+                console.log("destroy dock:", DOCK_TYPE);
+            }
+        });
+
+        console.log(this.i18n.helloPlugin);
+    }
+
+    onLayoutReady() {
+        this.loadData(STORAGE_NAME);
+    }
+
+    onunload() {
+        console.log(this.i18n.byePlugin);
+    }
+
+    openSetting() {
+        const dialog = new Dialog({
+            title: this.name,
+            content: `<div class="b3-dialog__content"><textarea class="b3-text-field fn__block" placeholder="readonly text in the menu"></textarea></div>
+<div class="b3-dialog__action">
+    <button class="b3-button b3-button--cancel">${this.i18n.cancel}</button><div class="fn__space"></div>
+    <button class="b3-button b3-button--text">${this.i18n.save}</button>
+</div>`,
+            width: isMobile() ? "92vw" : "520px",
+        });
+        const inputElement = dialog.element.querySelector("textarea");
+        inputElement.value = this.data[STORAGE_NAME].readonlyText;
+        const btnsElement = dialog.element.querySelectorAll(".b3-button");
+        dialog.bindInput(inputElement, () => {
+            (btnsElement[1] as HTMLButtonElement).click();
+        });
+        inputElement.focus();
+        btnsElement[0].addEventListener("click", () => {
+            dialog.destroy();
+        });
+        btnsElement[1].addEventListener("click", () => {
+            this.saveData(STORAGE_NAME, {readonlyText: inputElement.value});
+            dialog.destroy();
+        });
+    }
+
+    private wsEvent({detail}: any) {
+        console.log(detail);
+    }
+
+    private blockIconEvent({detail}: any) {
+        detail.menu.addSeparator(0);
+        const ids: string[] = [];
+        detail.blockElements.forEach((item: HTMLElement) => {
+            ids.push(item.getAttribute("data-node-id"));
+        });
+        detail.menu.addItem({
+            index: 1,
+            iconHTML: "",
+            type: "readonly",
+            label: "IDs<br>" + ids.join("<br>"),
+        });
+    }
+
+    private async addMenu(rect: DOMRect) {
+        const menu = new Menu("topBarSample", () => {
+            console.log(this.i18n.byeMenu);
+        });
+        menu.addItem({
+            icon: "iconHelp",
+            label: "Confirm",
+            click() {
+                confirm("Confirm", "Is this a confirm?", () => {
+                    showMessage("confirm");
+                }, () => {
+                    showMessage("cancel");
+                });
+            }
+        });
+        menu.addItem({
+            icon: "iconFeedback",
+            label: "Message",
+            click: () => {
+                showMessage(this.i18n.helloPlugin);
+            }
+        });
+        menu.addItem({
+            icon: "iconInfo",
+            label: "Dialog",
+            click: () => {
+                new Dialog({
+                    title: "Info",
+                    content: '<div class="b3-dialog__content">This is a dialog</div>',
+                    width: isMobile() ? "92vw" : "520px",
+                });
+            }
+        });
+        menu.addItem({
+            icon: "iconLayoutBottom",
+            label: "Open Tab",
+            click: () => {
+                openTab({
+                    custom: {
+                        icon: "iconEmoji",
+                        title: "Custom Tab",
+                        data: {
+                            text: "This is my custom tab",
+                        },
+                        fn: this.customTab
+                    },
+                });
+            }
+        });
+        menu.addItem({
+            icon: "iconLayout",
+            label: "Open Float Layer(open help)",
+            click: () => {
+                this.addFloatLayer({
+                    ids: ["20210428212840-8rqwn5o", "20201225220955-l154bn4"],
+                    defIds: ["20230415111858-vgohvf3", "20200813131152-0wk5akh"],
+                    x: window.innerWidth - 768 - 120,
+                    y: 32
+                });
+            }
+        });
+        menu.addItem({
+            icon: "iconTrashcan",
+            label: "Remove Data",
+            click: () => {
+                this.removeData(STORAGE_NAME).then(() => {
+                    this.data[STORAGE_NAME] = {readonlyText: "Readonly"};
+                });
+            }
+        });
+        menu.addItem({
+            icon: "iconScrollHoriz",
+            label: "Event Bus",
+            type: "submenu",
+            submenu: [{
+                icon: "iconSelect",
+                label: "On ws-main",
+                click: () => {
+                    this.eventBus.on("ws-main", this.wsEvent);
+                }
+            }, {
+                icon: "iconClose",
+                label: "Off ws-main",
+                click: () => {
+                    this.eventBus.off("ws-main", this.wsEvent);
+                }
+            }, {
+                icon: "iconSelect",
+                label: "On click-blockicon",
+                click: () => {
+                    this.eventBus.on("click-blockicon", this.blockIconEvent);
+                }
+            }, {
+                icon: "iconClose",
+                label: "Off click-blockicon",
+                click: () => {
+                    this.eventBus.off("click-blockicon", this.blockIconEvent);
+                }
+            }, {
+                icon: "iconSelect",
+                label: "On click-pdf",
+                click: () => {
+                    this.eventBus.on("click-pdf", this.wsEvent);
+                }
+            }, {
+                icon: "iconClose",
+                label: "Off click-pdf",
+                click: () => {
+                    this.eventBus.off("click-pdf", this.wsEvent);
+                }
+            }, {
+                icon: "iconSelect",
+                label: "On click-editorcontent",
+                click: () => {
+                    this.eventBus.on("click-editorcontent", this.wsEvent);
+                }
+            }, {
+                icon: "iconClose",
+                label: "Off click-editorcontent",
+                click: () => {
+                    this.eventBus.off("click-editorcontent", this.wsEvent);
+                }
+            }, {
+                icon: "iconSelect",
+                label: "On click-editortitleicon",
+                click: () => {
+                    this.eventBus.on("click-editortitleicon", this.wsEvent);
+                }
+            }, {
+                icon: "iconClose",
+                label: "Off click-editortitleicon",
+                click: () => {
+                    this.eventBus.off("click-editortitleicon", this.wsEvent);
+                }
+            }]
+        });
+        menu.addSeparator();
+        menu.addItem({
+            icon: "iconSparkles",
+            label: this.data[STORAGE_NAME].readonlyText || "Readonly",
+            type: "readonly",
+        });
+        if (isMobile()) {
+            menu.fullscreen();
+        } else {
+            menu.open({
+                x: rect.right,
+                y: rect.bottom,
+                isLeft: true,
+            });
+        }
+    }
+}
